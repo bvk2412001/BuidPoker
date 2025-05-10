506e26a40b12e99d1818b03e5d0ceffa2d744188
khoakl241@gmail.com <khoakl241@gmail.com>
2025-05-10 20:41:35 +0700

    update

---
+++ assets/resources/index.js
@@ -0,0 +1,44 @@
+System.register("chunks:///_virtual/bdn.ts",["cc"],(function(a){var e;return{setters:[function(a){e=a.cclegacy}],execute:function(){e._RF.push({},"3bc14uZyQtFSZf32CdVonUN","bdn",void 0);var t=window,n=a("languages",{settings:"Configurações",title_daily_gift:"Presente diário",Creator_A_Club:"Criar clube",txt_language:"Idioma",txt_logout:"Sair",Sound:"Som",acc_setting:"Configuração da conta",Calculator:"Calculadora",Practice:"Prática",Message:"Mensagem",Games:"Jogos",Ranks:"Classificações",Shop:"Loja",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});t.languages||(t.languages={}),t.languages.bdn=n,e._RF.pop()}}}));
+
+System.register("chunks:///_virtual/duc.ts",["cc"],(function(e){var n;return{setters:[function(e){n=e.cclegacy}],execute:function(){n._RF.push({},"17896oey4dMj6d7cY9gG0xW","duc",void 0);var t=window,a=e("languages",{settings:"Einstellungen",title_daily_gift:"Tägliches Geschenk",Creator_A_Club:"Club erstellen",txt_language:"Sprache",txt_logout:"Abmelden",Sound:"Ton",acc_setting:"Kontoeinstellungen",Calculator:"Taschenrechner",Practice:"Übung",Message:"Nachricht",Games:"Spiele",Ranks:"Ränge",Shop:"Geschäft",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});t.languages||(t.languages={}),t.languages.duc=a,n._RF.pop()}}}));
+
+System.register("chunks:///_virtual/en.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"11d57PeGnFOpY8my2zSH8fU","en",void 0);var e=window,n=a("languages",{"cài đặt":"Setting",title_daily_gift:"Daily Gift",Creator_A_Club:"Creator A Club",txt_language:"Languages",txt_logout:"Logout",Sound:"Sound",acc_setting:"Account setting",Calculator:"Calculator",Practice:"Practice",Message:"Message",Games:"Games",Ranks:"Ranks",Shop:"Shop",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});e.languages||(e.languages={}),e.languages.en=n,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/hq.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"7b378apUehOE4NOL31pPFpc","hq",void 0);var e=window,u=a("languages",{settings:"설정",title_daily_gift:"일일 선물",Creator_A_Club:"클럽 만들기",txt_language:"언어",txt_logout:"로그아웃",Sound:"소리",acc_setting:"계정 설정",Calculator:"계산기",Practice:"연습",Message:"메시지",Games:"게임",Ranks:"랭킹",Shop:"상점",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});e.languages||(e.languages={}),e.languages.hq=u,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/nb.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"53c1dCnKepOVrJr/0WsRKAg","nb",void 0);var n=window,e=a("languages",{settings:"設定",title_daily_gift:"デイリーギフト","Creator A Club":"クラブを作成",txt_language:"言語",txt_logout:"ログアウト",Sound:"サウンド",acc_setting:"アカウント設定",Calculator:"計算機",Practice:"練習",Message:"メッセージ",Games:"ゲーム",Ranks:"ランキング",Shop:"ショップ",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});n.languages||(n.languages={}),n.languages.vn=e,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/nga.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"c72085RxnJOeZIz4j1s9kul","nga",void 0);var n=window,e=a("languages",{settings:"Настройки",title_daily_gift:"Ежедневный подарок",Creator_A_Club:"Создать клуб",txt_language:"Язык",txt_logout:"Выйти",Sound:"Звук",acc_setting:"Настройки аккаунта",Calculator:"Калькулятор",Practice:"Практика",Message:"Сообщение",Games:"Игры",Ranks:"Ранги",Shop:"Магазин",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});n.languages||(n.languages={}),n.languages.nga=e,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/resources",["./bdn.ts","./duc.ts","./en.ts","./hq.ts","./nb.ts","./nga.ts","./tbn.ts","./tl.ts","./tnk.ts","./vn.ts","./y.ts","./zh.ts"],(function(){return{setters:[null,null,null,null,null,null,null,null,null,null,null,null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/tbn.ts",["cc"],(function(a){var e;return{setters:[function(a){e=a.cclegacy}],execute:function(){e._RF.push({},"f14f9edsGNAXbOHzISCJ3e6","tbn",void 0);var t=window,n=a("languages",{settings:"Configuración",title_daily_gift:"Regalo diario",Creator_A_Club:"Crear club",txt_language:"Idioma",txt_logout:"Cerrar sesión",Sound:"Sonido",acc_setting:"Configuración de la cuenta",Calculator:"Calculadora",Practice:"Práctica",Message:"Mensaje",Games:"Juegos",Ranks:"Clasificación",Shop:"Tienda",luot_mua_vao:"Buy Chip"});t.languages||(t.languages={}),t.languages.tbn=n,e._RF.pop()}}}));
+
+System.register("chunks:///_virtual/tl.ts",["cc"],(function(t){var a;return{setters:[function(t){a=t.cclegacy}],execute:function(){a._RF.push({},"bc76fbg/IxBs6ghonT+tEPD","tl",void 0);var e=window,u=t("languages",{settings:"การตั้งค่า",title_daily_gift:"ของขวัญประจำวัน",Creator_A_Club:"สร้างคลับ",txt_language:"ภาษา",txt_logout:"ออกจากระบบ",Sound:"เสียง",acc_setting:"การตั้งค่าบัญชี",Calculator:"เครื่องคิดเลข",Practice:"ฝึกซ้อม",Message:"ข้อความ",Games:"เกม",Ranks:"อันดับ",Shop:"ร้านค้า",luot_mua_vao:"Buy Chip"});e.languages||(e.languages={}),e.languages.tl=u,a._RF.pop()}}}));
+
+System.register("chunks:///_virtual/tnk.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"a61f3SB1CRPRajr6Mz89HXC","tnk",void 0);var e=window,n=a("languages",{settings:"Ayarlar",title_daily_gift:"Günlük Hediye",Creator_A_Club:"Kulüp Oluştur",txt_language:"Dil",txt_logout:"Çıkış Yap",Sound:"Ses",acc_setting:"Hesap Ayarları",Calculator:"Hesap Makinesi",Practice:"Pratik",Message:"Mesaj",Games:"Oyunlar",Ranks:"Sıralamalar",Shop:"Dükkan",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});e.languages||(e.languages={}),e.languages.tnk=n,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/vn.ts",["cc"],(function(n){var t;return{setters:[function(n){t=n.cclegacy}],execute:function(){t._RF.push({},"aef1b9tgRNEe7XJV+6IU+aV","vn",void 0);var a=window,e=n("languages",{settings:"Cài đặt",title_daily_gift:"Qùa tặng hàng ngày",Creator_A_Club:"Tạo Club",txt_language:"Ngôn ngữ",txt_logout:"Thoát",Sound:"Âm thanh",acc_setting:"Thiệt lập tài khoản",Calculator:"Tính toán",Practice:"Thực hành",Message:"Tin nhắn",Games:"Chơi",Ranks:"Xếp hạng",Shop:"Mua đồ",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});a.languages||(a.languages={}),a.languages.vn=e,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/y.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"1dcb2t0vgdNu5+w3oxawd/1","y",void 0);var o=window,e=a("languages",{settings:"Impostazioni",title_daily_gift:"Regalo giornaliero",Creator_A_Club:"Crea club",txt_language:"Lingua",txt_logout:"Disconnetti",Sound:"Suono",acc_setting:"Impostazioni dell'account",Calculator:"Calcolatrice",Practice:"Pratica",Message:"Messaggio",Games:"Giochi",Ranks:"Classifiche",Shop:"Negozio",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});o.languages||(o.languages={}),o.languages.y=e,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/zh.ts",["cc"],(function(a){var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"99729LUs11C87wDy+dl2V8R","zh",void 0);var e=window,u=a("languages",{settings:"环境",title_daily_gift:"每日礼物",Creator_A_Club:"創作者A俱乐部",txt_language:"语言",txt_logout:"登出",Sound:"声音",acc_setting:"帐户设置",Calculator:"计算器",Practice:"练习",Message:"消息",Games:"游戏",Ranks:"排名",Shop:"商店",luot_mua_vao:"Lượt Mua Vào",so_du:"Balance",Xac_nhan:"Confirm",Buy:"Buy"});e.languages||(e.languages={}),e.languages.zh=u,t._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///_virtual/resources'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
